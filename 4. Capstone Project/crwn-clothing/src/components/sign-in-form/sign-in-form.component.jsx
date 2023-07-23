cef71b3f0f85231d545e83490d0e38269ad9ccef
das972811 <salasdiego54@gmail.com>
2023-07-23 00:56:01 -0500

    Added UseContext for storing users

--- 4. Capstone Project/crwn-clothing/src/components/sign-in-form/sign-in-form.component.jsx
+++ 4. Capstone Project/crwn-clothing/src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,8 +1,10 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
 import { getRedirectResult } from 'firebase/auth';
 
 import FormInput from '../form-input/form-input.component';
 import Button from '../button/button.component';
+
+import { UserContext } from '../../contexts/user.context';
 
 import {
     auth,
@@ -23,6 +25,8 @@ const SignInForm = () => {
     const [formFields, setFormFields] = useState(defaultFormFields);
     const { email, password } = formFields;
 
+    const { setCurrentUser } = useContext(UserContext);
+
     useEffect(() => {
         getRedirectResult(auth).then(response => {
             if (response) {
@@ -39,8 +43,9 @@ const SignInForm = () => {
         event.preventDefault();
 
         try {
-            const response = await signInAuthUserWithEmailAndPassword(email, password);
-            console.log(response);
+            const { user } = await signInAuthUserWithEmailAndPassword(email, password);
+            setCurrentUser(user)
+
             resetFormFields();
         } catch (error) {
             switch (error.code) {

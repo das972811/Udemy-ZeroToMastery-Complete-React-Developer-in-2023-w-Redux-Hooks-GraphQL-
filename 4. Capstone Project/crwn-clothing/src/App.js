c6a189434d8cc4fddb58bddd93c1dea9a7b654be
das972811 <salasdiego54@gmail.com>
2023-07-24 00:33:09 -0500

    Finished with checkout page

--- 4. Capstone Project/crwn-clothing/src/App.js
+++ 4. Capstone Project/crwn-clothing/src/App.js
@@ -4,6 +4,7 @@ import Navigation from './routes/navigation/navigation.component';
 import Authentication from './routes/authentication/authentication.component';
 import Home from './routes/home/home.component';
 import Shop from './routes/shop/shop.component';
+import CheckOut from './routes/checkout/checkout.component';
 
 import './App.css';
 
@@ -12,9 +13,10 @@ const App = () => {
     return (
         <Routes>
             <Route path='/' element={<Navigation />}>
-                <Route index element={<Home />} />
-                <Route path='shop' element={<Shop />} />
-                <Route path='auth' element={<Authentication />} />
+                <Route index element={<Home />}/>
+                <Route path='shop' element={<Shop />}/>
+                <Route path='auth' element={<Authentication />}/>
+                <Route path='checkout' element={<CheckOut />}/>
             </Route>
         </Routes>
     );
--- 4. Capstone Project/crwn-clothing/src/components/cart-dropdown/cart-dropdown.component.jsx
+++ 4. Capstone Project/crwn-clothing/src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,4 +1,5 @@
 import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import { CartContext } from '../../contexts/cart.context';
 
@@ -9,7 +10,11 @@ import './cart-dropdown.styles.scss';
 
 const CartDropdown = () => {
     const { cartItems } = useContext(CartContext);
-    console.log(cartItems);
+    const navigate = useNavigate();
+
+    const goToCheckoutHandler = () => {
+        navigate('/checkout');
+    }
 
     return (
         <div className="cart-dropdown-container">
@@ -18,7 +23,7 @@ const CartDropdown = () => {
                     return <CartItem key={item.id} cartItem={item}/>
                 })}
             </div>
-            <Button>GO TO CHECKOUT</Button>
+            <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
         </div>
     );
 }
